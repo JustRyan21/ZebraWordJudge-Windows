7b756bd6e35f25864ba06471660ccb46320ed85b
JustRyan21 <77130963+JustRyan21@users.noreply.github.com>
2022-10-05 20:02:19 +1300

    Added font awesome and icons
    Added font awesome and icons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import im from '../images/Logo.png';
 import Logo from '../images/Logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Header(props) {
     return (
@@ -9,6 +11,7 @@ function Header(props) {
             <div className="header--div">
                 <Logo height={80}/>
                 <h2 className="header--title">Zebra Word Judge</h2>
+                <FontAwesomeIcon className="header--menu" icon={faBars} size="2x"/>
             </div>
             {/* <DropdownMenu lexiconData={props.lexiconData} setShowModal={props.setShowModal} currentLexicon={props.currentLexicon} setCurrentLexicon={(newLexicon) => props.setCurrentLexicon(newLexicon)} addLexicon={props.addLexicon} /> */}
       </header>
